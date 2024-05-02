3df4a7258f57bb0929925645e886a068b08ae1e9
Satya Ranjon <satyaranjonofficial@gmail.com>
2024-05-02 11:30:24 +0600

    serve

---
+++ index.js
@@ -0,0 +1,9 @@
+const jsonServer = require("json-server"); // importing json-server library
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(4000);
